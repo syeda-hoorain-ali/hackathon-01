a8c2b4216ce4daa69ebdc9785a533e12bc2a9b0f
syeda-hoorain-ali <syedahoorainali8@gmail.com>
2024-09-08 02:31:21 -0700

    Milestone 01

---
+++ milestone-01/dist/script.js
@@ -0,0 +1,12 @@
+"use strict";
+const h2s = document.querySelectorAll('h2');
+h2s.forEach(h2 => {
+    h2.addEventListener('click', (e) => {
+        const target = e.target;
+        const icon = target.firstElementChild;
+        const content = target.nextElementSibling;
+        content.classList.toggle('hidden');
+        icon.classList.toggle('fa-chevron-right');
+        icon.classList.toggle('fa-chevron-down');
+    });
+});
---
+++ milestone-01/src/script.ts
@@ -0,0 +1,14 @@
+const h2s = document.querySelectorAll('h2') as NodeListOf<HTMLHeadingElement>;
+
+h2s.forEach(h2 => {
+    h2.addEventListener('click', (e: MouseEvent) => {
+        const target = e.target as HTMLElement;
+
+        const icon = target.firstElementChild as HTMLElement;
+        const content = target.nextElementSibling as HTMLDivElement;
+
+        content.classList.toggle('hidden')
+        icon.classList.toggle('fa-chevron-right')
+        icon.classList.toggle('fa-chevron-down')
+    });
+});
