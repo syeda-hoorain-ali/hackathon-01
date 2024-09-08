0dbaad8d621a1c28b2453aa482dff220fae0b72d
syeda-hoorain-ali <syedahoorainali8@gmail.com>
2024-09-08 02:31:45 -0700

    Milestone 04

---
+++ milestone-04/src/types.ts
@@ -0,0 +1,14 @@
+export interface User {
+    username: string;
+    email: string;
+    name: string;
+    profilePic: string;
+    about: string;
+    phone: string;
+    city: string;
+    profession: string;
+    githubUrl: string;
+    linkedinUrl: string;
+    websiteUrl: string;
+}
+
